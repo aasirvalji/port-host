d5ac59c966d890eafe05768e3d9eded32c7c3936
aasirvalji <aasir.valji@hotmail.com>
2022-10-22 02:07:15 -0400

    added light assets and updated component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import meLight from '../assets/planetBlobLight.png';
 
 function Home() {
   return (
-    <div className='ml-4 flex justify-center items-center w-full h-screen'>
-      Summary of current
+    <div className='ml-4 w-full h-screen'>
+      <div className='home-container'>
+        <div className='home-image-container'>
+          <img src={meLight} alt='me' />
+        </div>
+        <div className='home-text-container dark:bg-gray-900 shadow-lg'>
+          <p>
+            {' '}
+            Hey, thanks for visiting my website! My name is Aasir Valji and im a
+            student at Western University. Im completing my fourth year of
+            Software Engineering and will be starting full time in the Summer of
+            2023.
+          </p>
+          <p>
+            {' '}
+            I've tried keeping this "portfolio" short and to the point but if
+            you want to chat more about anything you see here, feel free to
+            contact me on{' '}
+            <a
+              href='https://www.linkedin.com/in/aasir-valji/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fa-brands fa-linkedin'></i>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import amazonLight from '../assets/amazonLight.svg';
+import bellLight from '../assets/bellLight.png';
+import ibmLight from '../assets/ibmLight.png';
+
 function Work() {
   return (
-    <div className='ml-4 flex justify-center items-center w-full h-screen'>
-      Talk about my most recent work experiences
+    <div className='ml-4 flex flex-col justify-center items-center w-full h-screen'>
+      <p>Past places I've worked at</p>
+      <div className='company-cards'>
+        <div className='amazon-light-container dark:bg-gray-900 shadow-lg p-6'>
+          <img src={amazonLight} alt='amazon' />
+        </div>
+        <div className='bell-light-container dark:bg-gray-900 shadow-lg p-2.5'>
+          <img src={bellLight} alt='bell' />
+        </div>
+        <div className='ibm-light-container dark:bg-gray-900 shadow-lg'>
+          <img src={ibmLight} alt='ibm' />
+        </div>
+      </div>
     </div>
   );
 }
